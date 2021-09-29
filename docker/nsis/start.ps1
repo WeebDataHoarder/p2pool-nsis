4fdd2bf190e2087bc722a2466da75999692922ec
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-09-29 11:04:08 +0200

    Close p2pool parent if monerod closes, also do not allow concurrent instances at the same time of monerod/p2pool

--- docker/nsis/start.ps1
+++ docker/nsis/start.ps1
@@ -26,10 +26,24 @@ Function Get-Monero-Status
     return $response | ConvertFrom-Json-Compat
 }
 
+$p2pool = Get-Process -Name "p2pool" -ErrorAction SilentlyContinue
+if ($p2pool) {
+    Write-Host "Another instance of P2Pool exists, press enter to exit..."
+    Read-Host
+    Stop-Process -Id $Pid -Force
+}
+
+$monerod = Get-Process -Name "monerod" -ErrorAction SilentlyContinue
+if ($monerod) {
+    Write-Host "Another instance of monerod exists, press enter to exit..."
+    Read-Host
+    Stop-Process -Id $Pid -Force
+}
+
 $Wallet = "";
 $walletExists = Test-Path -Path "$dir\wallet.txt" -PathType leaf
 if ($walletExists) {
-    $Wallet = [IO.File]::ReadAllText("$dir\wallet.txt")
+    $Wallet = [System.IO.File]::ReadAllText("$dir\wallet.txt")
     $Wallet = $Wallet.Trim()
     Write-Output "Using stored wallet address on `"$($dir)\wallet.txt`": $($Wallet)"
 }
@@ -43,7 +57,13 @@ if ($Wallet -eq "") {
     [System.IO.File]::WriteAllLines("$($dir)\wallet.txt", $Wallet, $Utf8NoBomEncoding)
 }
 
-Start-Process .\monerod.exe -ArgumentList "--data-dir=.","--log-file","$dir\bitmonero.log","--enable-dns-blocklist","--zmq-pub=tcp://127.0.0.1:18083","--rpc-bind-ip=127.0.0.1","--rpc-bind-port=18081","--restricted-rpc","--disable-dns-checkpoints","--fast-block-sync=1", "--sync-pruned-blocks","--prune-blockchain","--check-updates disabled","--in-peers=8","--out-peers=16","--add-priority-node","node.supportxmr.com:18080"
+$monerod = Start-Process .\monerod.exe -PassThru -ArgumentList "--data-dir=.","--log-file","$dir\bitmonero.log","--enable-dns-blocklist","--zmq-pub=tcp://127.0.0.1:18083","--rpc-bind-ip=127.0.0.1","--rpc-bind-port=18081","--restricted-rpc","--disable-dns-checkpoints","--fast-block-sync=1", "--sync-pruned-blocks","--prune-blockchain","--check-updates disabled","--in-peers=8","--out-peers=16","--add-priority-node","node.supportxmr.com:18080"
+
+Register-ObjectEvent -InputObject $monerod -EventName exited -Action {
+    Stop-Process -Name "p2pool" -Confirm
+    Get-EventSubscriber | Unregister-Event
+    Stop-Process -Id $Pid -Force
+}
 
 Write-Output "Waiting for monerod to start and sync..."
 
